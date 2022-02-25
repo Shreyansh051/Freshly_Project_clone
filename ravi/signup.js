188dea9d3e9f271ebe30b40252c62b8c581dd56a
Ravipatel02 <87352218+Ravipatel02@users.noreply.github.com>
2022-02-25 12:58:28 +0530

    signup page

---
+++ ravi/signup.js
@@ -0,0 +1,29 @@
+document.querySelector('#form').addEventListener('submit',signup);
+
+var signupArr = JSON.parse(localStorage.getItem('registerdata'))||[];
+
+function signup(event){
+    event.preventDefault()
+
+    var name = document.querySelector('#name').value;
+    var mobile = document.querySelector("#mobile").value
+    var email = document.querySelector("#email").value
+    var pass = document.querySelector("#pass").value
+
+    var signupObj = {
+        name : name,
+        mobile:mobile,
+        email :email,
+        pass : pass,
+
+    }
+    //console.log(signupObj)
+    signupArr.push(signupObj)
+    //console.log(signupArr)
+    localStorage.setItem('registerdata',JSON.stringify(signupArr))
+
+    document.querySelector("input[type='submit']").addEventListener("click" , function(){
+        window.location.href = "login.html"
+    })
+
+}
